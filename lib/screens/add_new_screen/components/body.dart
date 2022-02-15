85468b0a9d17d9604547f441949e913d099ff935
minhho <minh.ho89@gmail.com>
2022-02-15 18:45:46 +0700

    add new product >>form making

--- lib/screens/add_new_screen/components/body.dart
+++ lib/screens/add_new_screen/components/body.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({Key? key}) : super(key: key);
 
   @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  final _keyForm = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Add new'));
+    return Form(
+        key: _keyForm,
+        child: ListView(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                label: Text('Title'),
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                label: Text('Price'),
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                label: Text('Decoration'),
+              ),
+            ),
+          ],
+        ));
   }
 }
