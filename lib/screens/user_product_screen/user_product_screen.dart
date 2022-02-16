3824f5748506f076f6051f788d6970179784d829
minhho <minh.ho89@gmail.com>
2022-02-16 12:12:07 +0700

    fetch and set added

--- lib/screens/user_product_screen/user_product_screen.dart
+++ lib/screens/user_product_screen/user_product_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app_nojson/consts/constants.dart';
+import 'package:shop_app_nojson/widgets/app_drawer.dart';
 
 import '../../providers/Products.dart';
 
@@ -11,43 +13,57 @@ class UserProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Products products = Provider.of<Products>(context, listen: false);
+    products.token = debugUserToken;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Products Management'),
+        title: const Text('Products Management'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => products.fetchAndSetProducts(),
             icon: Icon(Icons.add),
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: products.items.length,
-        itemBuilder: (context, index) => Card(
-          child: ListTile(
-            title: Text(products.items[index].title),
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(products.items[index].imageUrl),
-            ),
-            subtitle:
-                Text('\$${products.items[index].price.toStringAsFixed(2)}'),
-            trailing: FittedBox(
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.edit),
+      drawer: const AppDrawer(),
+      body: FutureBuilder(
+        future: products.fetchAndSetProducts(),
+        builder: (ctx, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? const Center(child: CircularProgressIndicator())
+                : RefreshIndicator(
+                    onRefresh: () => products.fetchAndSetProducts(),
+                    child: Consumer<Products>(
+                      builder: (ctx, productsData, _) => ListView.builder(
+                        itemCount: products.items.length,
+                        itemBuilder: (context, index) => Card(
+                          child: ListTile(
+                            title: Text(products.items[index].title),
+                            leading: CircleAvatar(
+                              backgroundImage:
+                                  NetworkImage(products.items[index].imageUrl),
+                            ),
+                            subtitle: Text(
+                                '\$${products.items[index].price.toStringAsFixed(2)}'),
+                            trailing: FittedBox(
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.edit),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.delete),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.delete),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
       ),
     );
   }
