b90f8f055ed431d6be0b90d83dbbfd418b695942
minhho <minh.ho89@gmail.com>
2022-02-18 22:12:01 +0700

    product card neumorphism

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app_nojson/consts/constants.dart';
+import 'package:shop_app_nojson/widgets/neumorphics/neumorphic_button.dart';
 
 import '../providers/Auth.dart';
 import '../providers/Cart.dart';
@@ -17,40 +18,53 @@ class ProductItem extends StatelessWidget {
     Cart cart = Provider.of<Cart>(context);
     final auth = Provider.of<Auth>(context);
 
-    return ClipRRect(
-      borderRadius: kProductCartBorderRadius,
-      child: GridTile(
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: Consumer<Product>(
-            builder: (context, product, child) => IconButton(
-                color: Theme.of(context).colorScheme.secondary,
+    return Container(
+      margin: EdgeInsets.only(top: 10.0),
+      padding: const EdgeInsets.all(8.0),
+      child: NeumorphicButton(
+        onPressed: () {},
+        borderRadius: kProductCartBorderRadius,
+        child: ClipRRect(
+          borderRadius: kProductCartBorderRadius,
+          child: GridTile(
+            footer: GridTileBar(
+              backgroundColor: kBackgroundColor,
+              leading: Consumer<Product>(
+                builder: (context, product, child) => IconButton(
+                    icon: Icon(
+                      product.isFavorite
+                          ? Icons.favorite
+                          : Icons.favorite_border,
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+                    onPressed: () => product.toggleFavoriteStatus(
+                        auth.token!, auth.userId!)),
+              ),
+              subtitle: Text(product.title),
+              trailing: IconButton(
                 icon: Icon(
-                  product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                  Icons.shopping_cart,
+                  color: Theme.of(context).primaryColor,
                 ),
-                onPressed: () =>
-                    product.toggleFavoriteStatus(auth.token!, auth.userId!)),
-          ),
-          subtitle: Text(product.title),
-          trailing: IconButton(
-            icon: Icon(
-              Icons.shopping_cart,
-              color: Theme.of(context).primaryColor,
+                onPressed: () {
+                  cart.addItem(product.id, product.price, product.title);
+                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: const Text('Item added'),
+                      duration: const Duration(seconds: 2),
+                      action: SnackBarAction(
+                        label: 'UNDO',
+                        onPressed: () => cart.removeAddedItem(product.id),
+                      )));
+                },
+              ),
             ),
-            onPressed: () {
-              cart.addItem(product.id, product.price, product.title);
-              ScaffoldMessenger.of(context).hideCurrentSnackBar();
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content: const Text('Item added'),
-                  duration: const Duration(seconds: 2),
-                  action: SnackBarAction(
-                    label: 'UNDO',
-                    onPressed: () => cart.removeAddedItem(product.id),
-                  )));
-            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
-        child: Image.network(product.imageUrl),
       ),
     );
   }
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app_nojson/consts/constants.dart';
-import 'package:shop_app_nojson/widgets/neumorphics/neumorphic_button.dart';
 
 import '../providers/Products.dart';
 import 'product_item.dart';
@@ -18,23 +16,16 @@ class ProductsGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     var productsData = Provider.of<Products>(context);
     final products = showFav ? productsData.favoriteItems : productsData.items;
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: GridView.builder(
-        itemCount: products.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            childAspectRatio: 2 / 3),
-        itemBuilder: (context, i) => NeumorphicButton(
-          borderRadius: kProductCartBorderRadius,
-          onPressed: () {},
-          child: ChangeNotifierProvider.value(
-            value: products[i],
-            child: const ProductItem(),
-          ),
-        ),
+    return GridView.builder(
+      itemCount: products.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          childAspectRatio: 2 / 3),
+      itemBuilder: (context, i) => ChangeNotifierProvider.value(
+        value: products[i],
+        child: const ProductItem(),
       ),
     );
   }
