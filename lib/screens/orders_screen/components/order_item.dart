b4c05c8bea24d121bb83a360c5f69a9c05d29e6a
minhho <minh.ho89@gmail.com>
2022-02-19 15:07:29 +0700

    order item card expanding

--- lib/screens/orders_screen/components/order_item.dart
+++ lib/screens/orders_screen/components/order_item.dart
@@ -26,19 +26,79 @@ class _OrderItemState extends State<OrderItem> {
       shadowBlur: 10,
       borderRadius: BorderRadius.circular(10),
       backgroundColor: kBackgroundColor,
-      child: ListTile(
-        title: Text('${widget.order.amount}\$'),
-        subtitle: Text(
-          DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
-        ),
-        trailing: IconButton(
-          icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-          onPressed: () {
-            setState(() {
-              _expanded = !_expanded;
-            });
-          },
-        ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('${widget.order.amount}\$'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+            ),
+            trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
+          ),
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            height:
+                _expanded ? (widget.order.products.length * 20.0 + 10.0) : 0,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: ListView(
+                children: widget.order.products
+                    .map((product) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              width: 80,
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Colors.black),
+                              ),
+                              child: Text(
+                                product.title,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Theme.of(context)
+                                      .textTheme
+                                      .titleSmall!
+                                      .fontSize,
+                                ),
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Container(
+                                    decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.black),
+                                    ),
+                                    width: 80,
+                                    child: Text('\$${product.price}')),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Text('x${product.quantity}'),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text('='),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text('${product.price * product.quantity}'),
+                              ],
+                            ),
+                          ],
+                        ))
+                    .toList(),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
