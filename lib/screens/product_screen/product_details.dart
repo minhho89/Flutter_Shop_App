174c5d2317bd02c3339d67c3142ba9b24737fb41
minhho <minh.ho89@gmail.com>
2022-02-24 11:38:38 +0700

    product details screen

--- lib/screens/product_screen/product_details.dart
+++ lib/screens/product_screen/product_details.dart
@@ -17,77 +17,85 @@ class ProductDetailsScreen extends StatelessWidget {
 
     return Scaffold(
         backgroundColor: kBackgroundColor,
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              expandedHeight: MediaQuery.of(context).size.height * 1 / 3,
-              pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Hero(
-                  tag: loadedProduct.id,
-                  child: Image.network(
-                    loadedProduct.imageUrl,
-                    fit: BoxFit.cover,
+        body: SafeArea(
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                leading: BackButton(
+                  color: Theme.of(context).primaryColor,
+                ),
+                backgroundColor: kBackgroundColor,
+                expandedHeight: MediaQuery.of(context).size.height * 1 / 3,
+                pinned: false,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Hero(
+                    tag: loadedProduct.id,
+                    child: Image.network(
+                      loadedProduct.imageUrl,
+                      fit: BoxFit.scaleDown,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SliverList(
-                delegate: SliverChildListDelegate([
-              NeumorphicCard(
-                borderRadius: BorderRadius.circular(13),
-                shadowBlur: 13,
-                backgroundColor: kBackgroundColor,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Text(
-                        loadedProduct.title,
-                        style: TextStyle(
-                          fontSize:
-                              Theme.of(context).textTheme.titleMedium!.fontSize,
+              SliverList(
+                  delegate: SliverChildListDelegate([
+                NeumorphicCard(
+                  borderRadius: BorderRadius.circular(13),
+                  shadowBlur: 13,
+                  backgroundColor: kBackgroundColor,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Text(
+                          loadedProduct.title,
+                          style: TextStyle(
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .fontSize,
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 10),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 10),
+                        child: Text(
+                          '\$${loadedProduct.price}',
+                          style: TextStyle(
+                              fontSize: Theme.of(context)
+                                  .textTheme
+                                  .headline4!
+                                  .fontSize),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: Card(
+                    elevation: 0,
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
                       child: Text(
-                        '\$${loadedProduct.price}',
+                        loadedProduct.description,
                         style: TextStyle(
                             fontSize: Theme.of(context)
                                 .textTheme
-                                .headline4!
+                                .titleMedium!
                                 .fontSize),
                       ),
                     ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: Card(
-                  elevation: 0,
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Text(
-                      'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
-                      style: TextStyle(
-                          fontSize: Theme.of(context)
-                              .textTheme
-                              .titleMedium!
-                              .fontSize),
-                    ),
                   ),
-                ),
-              )
-            ]))
-          ],
+                )
+              ]))
+            ],
+          ),
         ));
   }
 }
